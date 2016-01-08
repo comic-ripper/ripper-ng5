1d83172aa0402cdbf37ff6bc47ad22486b64c31e
Eric Nelson <enelson@eligoenergy.com>
2016-01-08 17:07:02 -0600

    Better production config, don't include compiled js

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,6 +21,11 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: '"production"' },
     }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
   ],
   module: {
     loaders: [
