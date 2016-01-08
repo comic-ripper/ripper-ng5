89fcba59fb9053e773ba86217c193185ca84b140
Eric Nelson <enelson@eligoenergy.com>
2016-01-08 16:48:32 -0600

    fixed production webpack config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,16 +5,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   devtool: 'source-map',
   entry: [
-    './src/index',
+    './app/js/index',
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Ripper',
-      template: path.join(__dirname, 'src', 'index.html'),
+      template: path.join(__dirname, 'app', 'templates', 'index.html'),
       hash: true,
     }),
     new webpack.NoErrorsPlugin(),
@@ -28,7 +28,7 @@ module.exports = {
         test: /\.js$/,
         loaders: ['babel'],
         exclude: /node_modules/,
-        include: path.join(__dirname, 'src'),
+        include: path.join(__dirname, 'app'),
       },
       {
         test: /\.scss$/,
